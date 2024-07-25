9ffb66f5a760524b089090fd137d4ac6dc74a4aa
Matheus Pires <mopires23@gmail.com>
2024-07-25 19:28:31 -0300

    Fix lonewolfes

--- src/modules/Tokenizer.ts
+++ src/modules/Tokenizer.ts
@@ -39,185 +39,194 @@ class Tokenizer {
     //FileContent = Array.from(FileContent);
 
     while (this.column < FileContent.length) {
-      switch (FileContent[this.column]) {
-        case "/":
-          if (FileContent[this.column + 1] === "/") {
-            let comment = FileContent[this.column];
+      if (!this.isSpecialCharacter(FileContent[this.column])) {
+        switch (FileContent[this.column]) {
+          case "/":
+            if (FileContent[this.column + 1] === "/") {
+              let comment = FileContent[this.column];
+              while (true) {
+                this.nextColumn();
+                if (FileContent[this.column] !== os.EOL) {
+                  comment = comment + FileContent[this.column];
+                } else {
+                  break;
+                }
+              }
+              this.syntaxExpression.push({
+                Comment: comment,
+                Column: this.column,
+                Line: this.line
+              });
+              this.nextColumn();
+            } else {
+              this.char_buffer += FileContent[this.column];
+              this.nextColumn();
+            }
+            break;
+          case " ":
+            if (this.char_buffer.length > 0) {
+              this.token_id = tokenId(this.char_buffer);
+              this.syntaxExpression.push({
+                Symbol: this.char_buffer,
+                Column: this.column,
+                Line: this.line
+              });
+              this.char_buffer = "";
+            }
+            this.nextColumn();
+            break;
+          case "\n":
+            this.line = this.line + 1;
+            if (this.char_buffer.length > 0) {
+              if (this.char_buffer.match("close") === null) {
+                this.syntaxExpression.push({
+                  Symbol: this.char_buffer,
+                  Column: this.column,
+                  Line: this.line
+                });
+                this.char_buffer = "";
+              } else {
+                let close_tag = this.char_buffer.split("close")[1];
+                this.syntaxExpression.forEach((element) => {
+                  if (element.Symbol !== undefined) {
+                    if (element.Symbol == close_tag) {
+                      element.CloseTag = this.char_buffer;
+                    }
+                  }
+                });
+                this.syntaxExpression.push({
+                  Symbol: this.char_buffer,
+                  Column: this.column,
+                  Line: this.line
+                });
+                this.char_buffer = "";
+              }
+            }
+            this.nextColumn();
+            break;
+          case "\r":
+            this.line = this.line + 1;
+            if (this.char_buffer.length > 0) {
+              if (this.char_buffer.match("close") === null) {
+                this.syntaxExpression.push({
+                  Symbol: this.char_buffer,
+                  Column: this.column,
+                  Line: this.line
+                });
+                this.char_buffer = "";
+              } else {
+                let close_tag = this.char_buffer.split("close")[1];
+                this.syntaxExpression.forEach((element) => {
+                  if (element.Symbol !== undefined) {
+                    if (element.Symbol == close_tag) {
+                      element.CloseTag = this.char_buffer;
+                    }
+                  }
+                });
+                this.syntaxExpression.push({
+                  Symbol: this.char_buffer,
+                  Column: this.column,
+                  Line: this.line
+                });
+                this.char_buffer = "";
+              }
+            }
+            this.nextColumn();
+            break;
+          case "=":
+            this.getAttribute(FileContent);
+            this.nextColumn();
+            break;
+          case "'":
+            let single_quotes_content = FileContent[this.column];
             while (true) {
               this.nextColumn();
-              if (FileContent[this.column] !== os.EOL) {
-                comment = comment + FileContent[this.column];
-              } else {
+              single_quotes_content =
+                single_quotes_content + FileContent[this.column]; //send to expression
+              if (FileContent[this.column] === "'") {
                 break;
               }
             }
-            this.syntaxExpression.push({
-              Comment: comment,
-              Column: this.column,
-              Line: this.line
-            });
-            this.nextColumn();
-          } else {
-            this.char_buffer += FileContent[this.column];
-            this.nextColumn();
-          }
-          break;
-        case " ":
-          if (this.char_buffer.length > 0) {
             this.token_id = tokenId(this.char_buffer);
             this.syntaxExpression.push({
-              Symbol: this.char_buffer,
-              Column: this.column,
-              Line: this.line
+              Symbol: "single_quotes",
+              String: single_quotes_content,
+              Column: this.column
             });
-            this.char_buffer = "";
-          }
-          this.nextColumn();
-          break;
-        case "\n":
-          this.line = this.line + 1;
-          if (this.char_buffer.length > 0) {
-            if (this.char_buffer.match("close") === null) {
-              this.syntaxExpression.push({
-                Symbol: this.char_buffer,
-                Column: this.column,
-                Line: this.line
-              });
-              this.char_buffer = "";
-            } else {
-              let close_tag = this.char_buffer.split("close")[1];
-              this.syntaxExpression.forEach((element) => {
-                if (element.Symbol !== undefined) {
-                  if (element.Symbol == close_tag) {
-                    element.CloseTag = this.char_buffer;
-                  }
-                }
-              });
-              this.syntaxExpression.push({
-                Symbol: this.char_buffer,
-                Column: this.column,
-                Line: this.line
-              });
-              this.char_buffer = "";
-            }
-          }
-          this.nextColumn();
-          break;
-        case "\r":
-          this.line = this.line + 1;
-          if (this.char_buffer.length > 0) {
-            if (this.char_buffer.match("close") === null) {
-              this.syntaxExpression.push({
-                Symbol: this.char_buffer,
-                Column: this.column,
-                Line: this.line
-              });
-              this.char_buffer = "";
-            } else {
-              let close_tag = this.char_buffer.split("close")[1];
-              this.syntaxExpression.forEach((element) => {
-                if (element.Symbol !== undefined) {
-                  if (element.Symbol == close_tag) {
-                    element.CloseTag = this.char_buffer;
-                  }
-                }
-              });
-              this.syntaxExpression.push({
-                Symbol: this.char_buffer,
-                Column: this.column,
-                Line: this.line
-              });
-              this.char_buffer = "";
-            }
-          }
-          this.nextColumn();
-          break;
-        case "=":
-          this.getAttribute(FileContent);
-          this.nextColumn();
-          break;
-        case "'":
-          let single_quotes_content = FileContent[this.column];
-          while (true) {
             this.nextColumn();
-            single_quotes_content =
-              single_quotes_content + FileContent[this.column]; //send to expression
-            if (FileContent[this.column] === "'") {
-              break;
+            break;
+          case '"':
+            let quotes_content = FileContent[this.column];
+            while (true) {
+              this.nextColumn();
+              quotes_content = quotes_content + FileContent[this.column]; //send to expression
+              if (FileContent[this.column] === '"') {
+                break;
+              }
             }
-          }
-          this.token_id = tokenId(this.char_buffer);
-          this.syntaxExpression.push({
-            Symbol: "single_quotes",
-            String: single_quotes_content,
-            Column: this.column
-          });
-          this.nextColumn();
-          break;
-        case '"':
-          let quotes_content = FileContent[this.column];
-          while (true) {
-            this.nextColumn();
-            quotes_content = quotes_content + FileContent[this.column]; //send to expression
-            if (FileContent[this.column] === '"') {
-              break;
-            }
-          }
-          this.token_id = tokenId(this.char_buffer);
-          this.syntaxExpression.push({
-            Symbol: "quotes",
-            String: quotes_content,
-            Column: this.column
-          });
-          this.parent_symbol = this.token_id;
-          this.nextColumn();
-          break;
-        case "$":
-          let $var = FileContent[this.column];
-          let column_backup = this.column;
-          while (true) {
-            this.nextColumn();
-            $var = $var + FileContent[this.column];
-            if (
-              FileContent[this.column] === " " ||
-              FileContent[this.column] === "\r" ||
-              FileContent[this.column] === "\n"
-            ) {
-              break;
-            } else if (
-              FileContent[this.column].match(/[$&+,:;=?@#|'<>.^*()%!-]/)
-            ) {
-              log(
-                chalk.red(
-                  `* Can't use this special chars in variables. At line ${this.line}`
-                )
-              );
-              process.exit();
-            }
-          }
-          if (this.variablesExist($var)) {
+            this.token_id = tokenId(this.char_buffer);
             this.syntaxExpression.push({
-              Symbol: "$",
-              FreeText: this.GetVariableValue($var),
-              Column: this.column,
-              Line: this.line
+              Symbol: "quotes",
+              String: quotes_content,
+              Column: this.column
             });
-          }
-          break;
+            this.parent_symbol = this.token_id;
+            this.nextColumn();
+            break;
+          case "$":
+            console.log("special chars");
+            this.nextColumn();
+            break;
+            let $var = FileContent[this.column];
+            let column_backup = this.column;
+            while (true) {
+              this.nextColumn();
+              $var = $var + FileContent[this.column];
+              if (
+                FileContent[this.column] === " " ||
+                FileContent[this.column] === "\r" ||
+                FileContent[this.column] === "\n"
+              ) {
+                break;
+              } else if (
+                FileContent[this.column].match(/[$&+,:;=?@#|'<>.^*()%!-]/)
+              ) {
+                log(
+                  chalk.red(
+                    `* Can't use this special chars in variables. At line ${this.line}`
+                  )
+                );
+                process.exit();
+              }
+            }
+            if (this.variablesExist($var)) {
+              this.syntaxExpression.push({
+                Symbol: "$",
+                FreeText: this.GetVariableValue($var),
+                Column: this.column,
+                Line: this.line
+              });
+            }
+            break;
 
-        default:
-          this.char_buffer = this.char_buffer + FileContent[this.column];
-          if (FileContent[this.column + 1] === undefined) {
-            this.token_id = tokenId(this.char_buffer);
-            this.syntaxExpression.push({
-              Symbol: this.char_buffer,
-              Column: this.column,
-              Line: this.line
-            });
-            this.char_buffer = "";
-          }
-          this.nextColumn();
-          break;
+          default:
+            // If none of the condition is met,
+            // It's a tag and will be added to the buffer
+            this.char_buffer = this.char_buffer + FileContent[this.column];
+            if (FileContent[this.column + 1] === undefined) {
+              this.token_id = tokenId(this.char_buffer);
+              this.syntaxExpression.push({
+                Symbol: this.char_buffer,
+                Column: this.column,
+                Line: this.line
+              });
+              this.char_buffer = "";
+            }
+            this.nextColumn();
+            break;
+        }
+      } else {
+        this.nextColumn();
       }
     }
     return this.syntaxExpression;
@@ -280,6 +289,10 @@ class Tokenizer {
     );
 
     return variable_file[variable].toString();
+  }
+
+  isSpecialCharacter(character: string) {
+    return "!@#$%¨&*()_+`{^}:><¹²³£¢¬§ªº╚├?°₢\\".includes(character);
   }
 }
 
